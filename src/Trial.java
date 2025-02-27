df6e79cd322e35a71458ea4437dfbccfe718ce2f
Anshu-x <ansumanreet333@gmail.com>
2025-02-27 17:45:40 +0530

    New commit

--- src/Trial.java
+++ src/Trial.java
@@ -1,5 +1,5 @@
 public class Trial {
     public static void main(String[] args) {
-        System.out.println("Azrithyx , good to go!!");
+        System.out.println("Azrithyx , good to godfsd!!");
     }
 }
