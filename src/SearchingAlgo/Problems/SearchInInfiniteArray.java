8cc6e710110f18c8162f206713d773a726c41969
Anshu-x <ansumanreet333@gmail.com>
2025-03-03 13:16:11 +0530

    Split the function into two

--- src/SearchingAlgo/Problems/SearchInInfiniteArray.java
+++ src/SearchingAlgo/Problems/SearchInInfiniteArray.java
@@ -1,34 +1,45 @@
 package SearchingAlgo.Problems;
 
+import SearchingAlgo.BinarySearch;
+
 public class SearchInInfiniteArray {
     public static void main(String[] args) {
         int[] arr = {-34, -17, -4, 0, 1, 3, 5, 7, 8, 12, 23, 34, 56, 67, 123, 234, 564};
-        SearchingAlgo.BinarySearch.printIndex(arr);
-        System.out.println(binSearchIncr(arr, 56));
-//        System.out.println(binSearchIncr(arr, 564));
-//        System.out.println(binSearchIncr(arr, -34));
-//        System.out.println(binSearchIncr(arr, 2));
-//        System.out.println(binSearchIncr(arr, 8));
+        BinarySearch.printIndex(arr);
+        System.out.println(SearchInInfiniteArray.ans(arr, 56));
+//        System.out.println(SearchInInfiniteArray.ans(arr, 564));
+//        System.out.println(SearchInInfiniteArray.ans(arr, -34));
+//        System.out.println(SearchInInfiniteArray.ans(arr, 2));
+//        System.out.println(SearchInInfiniteArray.ans(arr, 8));
     }
 
-    static int binSearchIncr(int[] arr, int target) {
-        int start = 0;
+
+    static int ans(int[] arr, int target){
+        int start = 0 ;
         int end = 1;
+
+        while(target > arr[end]){
+            int newStart = end + 1;
+
+            end += (end - start + 1) * 2;
+            start = newStart;
+
+        }
+
+        return ans(arr, target, start,end);
+    }
+
+    static int ans(int[] arr, int target, int start, int end) {
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (target > arr[end]) {
-                end *= 2;
-            } else {
-
-                if (target == arr[mid]) {
-                    return mid;
-                } else if (target < arr[mid]) {
-                    end = mid - 1;
-                } else if (target > arr[mid]) {
-                    start = mid + 1;
-                }
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
             }
 
 
