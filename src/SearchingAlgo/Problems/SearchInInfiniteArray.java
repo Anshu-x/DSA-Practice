6975495bbde2193ec4ffdd15681ee5e431b15101
Anshu-x <ansumanreet333@gmail.com>
2025-03-03 12:37:04 +0530

    Searching in infinite array using BS

---
+++ src/SearchingAlgo/Problems/SearchInInfiniteArray.java
@@ -0,0 +1,38 @@
+package SearchingAlgo.Problems;
+
+public class SearchInInfiniteArray {
+    public static void main(String[] args) {
+        int[] arr = {-34,-17,-4,0,1,3,5,7,8,12,23,34,56,67,123,234,564};
+        SearchingAlgo.BinarySearch.printIndex(arr);
+        System.out.println(binSearchIncr(arr, 56));
+//        System.out.println(binSearchIncr(arr, 564));
+//        System.out.println(binSearchIncr(arr, -34));
+//        System.out.println(binSearchIncr(arr, 2));
+//        System.out.println(binSearchIncr(arr, 8));
+    }
+
+    static int binSearchIncr(int[] arr, int target){
+        int start = 0;
+        int end = 1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(target == arr[mid]) {
+                return mid;
+            }
+            else if(target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            }
+
+            if(start > end){
+                end *= 2 ;
+            }
+
+        }
+        return -1;
+    }
+}
